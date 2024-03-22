6025134b64cc1e765a73bc631bdcfd3d73a709b1
sebastiancc27 <sebasccastizo27@gmail.com>
2024-03-22 11:39:00 -0600

    Practica00

--- practica00/src/main/java/mx/uv/App.java
+++ practica00/src/main/java/mx/uv/App.java
@@ -1,6 +1,6 @@
 package mx.uv;
 import static spark.Spark.*;
-
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 /**
  * Hello world!
  *
@@ -11,6 +11,54 @@ public class App
     {
         System.out.println( "Hello World!" );
 
-        get("/", (request,response)->"hola");
+        get("/", (request,response)->{
+            String respuesta="<note>\r\n" + //
+                                "  <to>Tove</to>\r\n" + //
+                                "  <from>Jani</from>\r\n" + //
+                                "  <heading>Reminder</heading>\r\n" + //
+                                "  <body>Don't forget me this weekend!</body>\r\n" + //
+                                "</note>";
+            response.type("application/xml");
+            return respuesta;
+        });
+
+        get("/xml", (request, response)->{
+            Alumno alumno=new Alumno();
+            alumno.setNombre("Sebastian");
+            alumno.setId("7");
+            XmlMapper xml = new XmlMapper();
+            String  respuesta=xml.writeValueAsString(alumno);
+
+            response.type("application/xml");
+
+            return respuesta;
+            //return "<nombre>"+respuesta.getNombre()+"</nombre>";
+        });
     }
+}
+
+class Alumno{
+    String nombre;
+    String apellido;
+    String id;
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    
 }
